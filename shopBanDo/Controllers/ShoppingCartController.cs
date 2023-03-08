954e3baae3a3966cd64c9fc66830e6e9c73e2006
Hiephuynh200 <huynhhiepvan1998@gmail.com>
2023-03-08 08:20:35 +0700

    8/3_Hiep

--- shopBanDo/Controllers/ShoppingCartController.cs
+++ shopBanDo/Controllers/ShoppingCartController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using shopBanDo.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,8 @@ namespace shopBanDo.Controllers
     public class ShoppingCartController : Controller
     {
         // GET: ShoppingCart
+        MyDataDataContext data = new MyDataDataContext();
+
         public ActionResult Index()
         {
             return View();
